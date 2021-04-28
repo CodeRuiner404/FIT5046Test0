d3e8d9723676f601205e1a6447e4ff0b2c2edbe6
Ziyi Zhu <zzhu0034@student.monash.edu>
2021-04-28 22:28:39 +0800

    Add sign, but it need more change

---
+++ app/src/main/java/com/example/fit5046test0/room/PainRecord.java
@@ -0,0 +1,29 @@
+package com.example.fit5046test0.room;
+
+import java.util.Date;
+
+public class PainRecord {
+
+    private int recordID;
+    //pde
+    private int painIntensityLevel;
+    private String painLocation;
+    private int moodLevel;
+    private int steps;
+    private Date date;
+
+    //weather
+    private double temperature;
+    private double humidity;
+    private double pressure;
+
+    //user
+    private String userEmail;
+    
+
+
+
+
+
+
+}
